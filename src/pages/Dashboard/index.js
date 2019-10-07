bb1d3c6aff0b9b8b05847245734903b2c76ab664
Rafael Marinho Farias <rafael_mf3@hotmail.com>
2019-10-06 23:28:17 -0300

    websocket

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,25 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useMemo} from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 
 import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
   
+  const user_id = localStorage.getItem('user');
+  const socket = useMemo(() => socketio('http://localhost:3333', {
+    query: { user_id }
+  }), [user_id]);
+
+  useEffect(() => {
+    socket.on('booking_request', data => {
+      setRequests([...requests, data]);
+    })
+  }, [requests, socket])
+
   useEffect(() => {
     const user_id = localStorage.getItem('user');
     api.get('/dashboard', {
@@ -15,9 +28,32 @@ export default function Dashboard() {
       setSpots(response.data);
     });
   }, []);
+
+  async function handleAccept(id) {
+    await api.post(`/bookings/${id}/approvals`);
+
+    setRequests(requests.filter(request => request._id !== id ));
+  }
+
+  async function handleReject(id) {
+    await api.post(`/bookings/${id}/rejections`)
+  
+    setRequests(requests.filter(request => request._id !== id ));
+  }
   
   return (
     <>
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong>
+            </p>
+            <button onClick={() => handleAccept(request._id)} className="accept">Aceitar</button>
+            <button onClick={() => handleReject(request._id)} className="reject">Rejeitar</button>
+          </li>
+        ))}
+      </ul>
       <ul className="spot-list">
         {spots.map(spots => (
           <li key={spots._id}>
